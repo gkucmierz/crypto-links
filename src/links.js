74243fd7594fb690d6247490bbce94dcb2336e9d
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-10-01 05:29:40 +0200

    add some links

--- src/links.js
+++ src/links.js
@@ -1,10 +1,26 @@
 
-const ETH = 'ETH';
-const BTC = 'BTC';
 const MEV = 'MEV';
 const WEB3 = 'web3';
 const SOLIDITY = 'solidity';
+const SATOSHI = 'satoshi';
+const CHARTS = 'charts';
+const ORDERBOOK = 'orderbook';
+const BLOCKCHAIN = 'blockchain';
+const BLOCK_EXPLORER = 'block explorer';
+const MEMPOOL = 'mempool';
+
+// categories:
 const TOOL = 'tool';
+const NEWS = 'news';
+const VISUALIZATION = 'visualization';
+
+// cc codes:
+const PL = 'PL';
+
+// currencies:
+const ETH = 'ETH';
+const BTC = 'BTC';
+const PLN = 'PLN';
 
 const BIP = n => `BIP ${n}`;
 
@@ -43,6 +59,66 @@ const links = [
     title: 'Chainlist',
     url: 'https://chainlist.org',
     keywords: [TOOL, ETH, WEB3, 'EVM', 'RPC', 'chain'],
+    description: '',
+  },
+  {
+    title: 'pln sat',
+    url: 'https://plnsat.com',
+    keywords: [PL, BTC, PLN, SATOSHI],
+    description: 'Ile satoshi jest warta 1 złotówka',
+  },
+  {
+    title: 'satoshis per person',
+    url: 'https://satoshisperperson.com',
+    keywords: [BTC, SATOSHI],
+    description: 'How many satoshis are there per person in the world',
+  },
+  {
+    title: 'look into bitcoin',
+    url: 'https://lookintobitcoin.com',
+    keywords: [BTC, CHARTS, 'indicator'],
+    description: 'Making Bitcoin data accessible to everyone',
+  },
+  {
+    title: 'coinlobster',
+    url: 'https://coinlobster.com',
+    keywords: [BTC, ORDERBOOK],
+    description: 'Combined Orderbook',
+  },
+  {
+    title: 'Bitcoin Legality Map',
+    url: 'https://www.bitrawr.com/terminal/legality-map',
+    keywords: [BTC, 'legal', 'illegal', 'alegal', 'legal tender'],
+    description: '',
+  },
+  {
+    title: 'rekt',
+    url: 'https://rekt.news',
+    keywords: [BTC, NEWS, 'rekt', 'hack'],
+    description: '',
+  },
+  {
+    title: 'sha256 algorithm',
+    url: 'https://sha256algorithm.com',
+    keywords: [BTC, TOOL, 'sha256'],
+    description: '',
+  },
+  {
+    title: 'Mampool space',
+    url: 'https://mempool.space',
+    keywords: [BTC, BLOCKCHAIN, BLOCK_EXPLORER, MEMPOOL],
+    description: '',
+  },
+  {
+    title: 'BitListen',
+    url: 'https://www.bitlisten.com',
+    keywords: [BTC, MEMPOOL, VISUALIZATION],
+    description: '',
+  },
+  {
+    title: 'BitBonkers',
+    url: 'https://bitbonkers.netlify.app',
+    keywords: [BTC, MEMPOOL, VISUALIZATION],
     description: '',
   },
   // {
