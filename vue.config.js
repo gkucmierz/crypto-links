5484e1f1e238e627fe03d6cd2d118c7d2997d667
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-10-01 04:14:45 +0200

    fix pwa icons

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,21 @@ module.exports = defineConfig({
       title: 'Crypto Links',
     }
   },
+  pwa: {
+    name: 'Crypto Links',
+    themeColor: '#FFD73F',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: './src/service-worker.js',
+      // ...other Workbox options...
+    },
+  },
   transpileDependencies: true,
   lintOnSave: false,
 });
