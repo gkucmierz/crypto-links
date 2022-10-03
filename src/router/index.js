74dab7cf61d1d1d575ad9a6ef6217a55a652d47a
Grzegorz Kućmierz <gkucmierz@gmail.com>
2022-10-03 19:50:27 +0200

    style active keywords

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+  },
+  {
+    path: '/filter/:keywords?',
+    name: 'filter',
+    component: HomeView,
   },
   {
     path: '/about',
@@ -14,6 +19,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/',
   }
 ]
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    <!-- {{ $route.params }} -->
+    <!-- {{ keywords }} -->
     <ul>
       <li
         v-for="(link, index) in links"
@@ -14,9 +16,11 @@
         <div class="keywords">
           <span
             class="keyword"
+            :class="{ active: hasKeyword(keyword) }"
             :style="keywordColor(keyword)"
             v-for="(keyword, ki) in link.keywords"
             v-bind:key="ki"
+            @click="filterKeyword(keyword)"
           >
             {{ keyword }}
           </span>
@@ -43,11 +47,33 @@ const colorHash = (() => {
   };
 })();
 
+const keywordsUrl = (() => {
+  const SEPARATOR = ',';
+  const WHITESPACE = '_';
+  return {
+    decode: str => {
+      const re = new RegExp(WHITESPACE, 'g');
+      return str.replace(re, ' ').split(SEPARATOR);
+    },
+    encode: keywords => {
+      const re = new RegExp(/ /, 'g');
+      return keywords.join(SEPARATOR).replace(re, WHITESPACE);
+    },
+  }
+})();
+
 export default {
   name: 'HomeView',
   data() {
+    const keywordsPhrase = this.$route.params.keywords;
+    const keywords = [];
+    if (keywordsPhrase) {
+      keywords.push(...keywordsUrl.decode(keywordsPhrase));
+    }
+    setTimeout(() => this.filterLinks());
     return {
       links: links,
+      keywords: [],
     }
   },
   methods: {
@@ -61,6 +87,34 @@ export default {
         '--dark-color': colorHash(keyword, 'dark'),
       };
     },
+    filterKeyword(keyword) {
+      const idx = this.keywords.indexOf(keyword);
+      if (idx !== -1) {
+        this.keywords.splice(idx, 1);
+      } else {
+        this.keywords.push(keyword);
+      }
+      const url = this.$router.resolve({ name: 'filter', params: { keywords: keywordsUrl.encode(this.keywords) } });
+      this.$router.push(url);
+      this.filterLinks();
+    },
+    filterLinks() {
+      this.links = links.filter(link => {
+        return this.keywords.every(keyword => {
+          return link.keywords.includes(keyword);
+        });
+      });
+    },
+    hasKeyword(keyword) {
+      return this.keywords.includes(keyword);
+    },
+  },
+  beforeRouteUpdate(to, from, next) {
+    if (to.params.keywords.length === 0) {
+      next('/');
+    } else {
+      next();
+    }
   },
 }
 </script>
@@ -97,6 +151,7 @@ ul {
         flex-wrap: wrap;
 
         .keyword {
+          cursor: pointer;
           background-color: var(--light-color);
           color: var(--dark-color);
           padding: 4px 8px;
@@ -108,6 +163,10 @@ ul {
           //   border: 1px solid var(--dark-color);
           //   margin: -1px;
           // }
+          &.active {
+            background-color: var(--dark-color);
+            color: var(--light-color);
+          }
         }
       }
 
